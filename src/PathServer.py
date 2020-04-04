401621321c4d8db868ab388fbac7ebdd081e6578
RichardHosea <richard.d.hosea@gmail.com>
2020-04-04 17:08:35 -0400

    fully working waypoint turtlebot code, need to integrate with simulation next

---
+++ src/PathServer.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from RBE550Project.srv import GetWaypoint
+from geometry_msgs.msg import Point, Twist
+import rospy
+import sys
+
+class PathServer():
+    def __init__(self, path):
+        rospy.init_node('path_server')
+        # Temporarily, we are making a point, for testing purposes. 
+        # Eventually, we will integrate this into the Simulation App
+        # TODO: Convert Node object into Point object!
+        
+        goal1 = Point()
+        goal1.x = .1
+        goal1.y = .1
+        
+        self.path = [goal1]
+        self.path.reverse()
+        
+        # Creating Server 
+        # GetWaypoint is an object with attribute Waypoint
+        self.server = rospy.Service('waypoint_service', GetWaypoint, self.path_server)
+        print("Server is created")
+        rospy.spin()
+
+    # callback to send back a goal message when requested by client
+    # we don't need data but when the callback is run it's given by default (it errors otherwise)
+    def path_server(self, data):
+        # if the path is not emtpy get next waypoint from the list of waypoints
+        if(len(self.path) is not 0):
+            waypoint = self.path.pop()
+            print ("Returning waypoint:", waypoint.x, waypoint.y)
+        else:
+            # return max int if there's no more points. ROS expects a Point message returned in every case
+            waypoint = Point(sys.maxsize, sys.maxsize, sys.maxsize)
+            
+        # returns an object of type GetWaypoint
+        # to get the waypoint itself, the controller calls [service_variable_name].Waypoint
+        return waypoint
+
+if __name__ == "__main__":
+    #Instantiates an object of type PathServer 
+    ps = PathServer([]) # needs waypoint list in parameter, empty for testing
--- src/turtlebotController.py
+++ src/turtlebotController.py
@@ -2,42 +2,55 @@
 import rospy
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
+from RBE550Project.srv import GetWaypoint
 from geometry_msgs.msg import Point, Twist
 import math
+import sys
 
 class TurtleBot():
     def __init__(self):
+        print("Waiting for server...")
+        rospy.wait_for_service('waypoint_service')
+        print("Connected to server")
+
+        self.nextWaypointProxy = rospy.ServiceProxy('waypoint_service', GetWaypoint)
+        print("Turtlebot waypoint client created")
+
+        # Initial position of TB, newOdom updates this anyway
         self.x = 0.0
         self.y = 0.0 
         self.theta = 0.0
-        self.distance = 0
-        self.wayPoints = []
+
+        # The distance to goal, which will be updated in update goal 
+        self.distanceToGoal = 0
+        
+        # flags to tell whether or not we have reached a goal
         self.rotatedFully = False
         self.reachedGoal = False
         self.newGoalRecieved =  False
+
+        #The heading to the new goal
+        self.headingToGoal = 0
+
+        # Initialize this goal
         rospy.init_node("turtlebot_controller")
 
+        # Subscriber for odometry and publisher for twist message
         self.sub = rospy.Subscriber("/odom", Odometry, self.newOdom)
         self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
 
+        # Run at 10Hz
         self.r = rospy.Rate(10)
+
+        # Starting point of the TB before it goes to next goal, updated in update goal
         self.start = Point()
         self.start.x = 0
         self.start.y = 0
-
-        self.goal = Point()
-        self.goal.x = 0
-        self.goal.y = 0
-
-        # goal1 = Point()
-        # goal1.x = -2
-        # goal1.y = -4
-
-        # goal2 = Point()
-        # goal2.x = 4 
-        # goal2.y = 4
-
-        #self.wayPoints = [goal1, goal2]
+        
+        # Next waypoint to go to
+        self.nextWaypoint = Point()
+        self.nextWaypoint.x = 0
+        self.nextWaypoint.y = 0
 
     #Recieves new odom information and updates robot pose
     def newOdom(self, msg):
@@ -49,82 +62,95 @@ class TurtleBot():
         rot_q = msg.pose.pose.orientation
         (roll, pitch, self.theta) = euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
 
+    # Rotates to a given heading 
     def rotateTo(self, angle):
-        #Goal heading is current theta + angle to rotate
         goalHeading = angle
         rotateSpeed = .15
+
         #Wrap around 
         if goalHeading > math.pi:
             goalHeading = goalHeading - 2*math.pi
         elif goalHeading < -1*math.pi:
             goalHeading = goalHeading + 2*math.pi
-        #While we haven't rotated fully
+
+        # find the error between our current theta and our desired heading
         error = goalHeading - self.theta
+        # if we haven't rotated fully
         if abs(error) > .01:
+            # Determines which way to turn
             if angle < 0:	    
                 self.publishTwist(0, -1*rotateSpeed)
                 self.rotatedFully = False
             else:
                 self.publishTwist(0, rotateSpeed)
                 self.rotatedFully = False
+        # if we have reached the goal, stop and set the flag accordingly
         else:
             self.publishTwist(0,0)
             self.rotatedFully = True
 
+    # This method publishes a twist message with a desired linear and angular velocity
     def publishTwist(self, linearVelocity, angularVelocity):
         try:
             msg = Twist()
             msg.linear.x = linearVelocity #set linear and angular velocities 
             msg.angular.z = angularVelocity
             self.pub.publish(msg) #publish
-            #print "Published!"
         except:
-            print "Failed to publish"
-
+            print ("Failed to publish")
+    
+    # Once the robot has rotated to the proper heading, this method will get it to the goal point
     def goToGoal(self, newGoal):
         speed = .15 
+        # calculates the distance moved since a new goal was set
         distanceMoved = (math.sqrt((self.start.y-self.y)**2 + (self.start.x-self.x)**2))
-        if(abs(distanceMoved) < self.distance): #check to see if distance has been reached 
+        # if we haven't moved the correct distance
+        if(abs(distanceMoved) < self.distanceToGoal): 
             self.publishTwist(speed,0) #publish a twist message to move forward 
             return False
+        # if we have reached the correct distance, stop
         else:
             self.publishTwist(0,0)
             return True
             
     def updateGoal(self, newGoal):
-        heading = math.atan2((newGoal.y-self.y),(newGoal.x-self.x))
-        print("Heading", heading)
-        self.rotateTo(heading)
+        # This method is called when we request a new waypoint. 
+        # It updates our start point (current point), distance to the goal, and heading to the goal
         self.start.x = self.x
         self.start.y = self.y
-        self.goal.x = newGoal.x
-        self.goal.y = newGoal.y
-        self.distance = math.sqrt((self.goal.y-self.start.y)**2 + (self.goal.x-self.start.x)**2)
-        print("Distance to goal", self.distance)
+        self.distanceToGoal = math.sqrt((newGoal.y-self.start.y)**2 + (newGoal.x-self.start.x)**2)
+        self.headingToGoal = math.atan2((newGoal.y-self.y),(newGoal.x-self.x))
+        print("Heading", self.headingToGoal)
+        print("Distance to goal", self.distanceToGoal)
 
 
+    def getWaypointClient(self):
+        try:
+            # get the next waypoint from the server. Waypoint is a Point message in the response message
+            self.nextWaypoint = self.nextWaypointProxy().Waypoint
+            print("Next waypoint", self.nextWaypoint.x,self.nextWaypoint.y)
+            #check if there's no more goals. In server if there's no more we send max int size
+            if(self.nextWaypoint.x > 1000000):
+                print("No more goals")
+            else:
+                # set the new goal as the next received waypoint
+                self.updateGoal(self.nextWaypoint)
+                # reset rotate and drive straight flags
+                self.rotatedFully = False
+                self.reachedGoal = False
+        except rospy.ServiceException as e:
+            print ("Service call failed: ")
 
-
-
-### Recieve new XY from Map
-### Calculate heading
-### Rotate to heading
-### Update start.x and start.y, goal.x and goal.y
-### Drive until reached goal
-
-goal1 = Point()
-goal1.x = -2
-goal1.y = -4
 TB = TurtleBot()
-heading = math.atan2((goal1.y-TB.y),(goal1.x-TB.x))
-print("Goal heading", heading)
-TB.distance = math.sqrt((goal1.y-TB.start.y)**2 + (goal1.x-TB.start.x)**2)
 while not rospy.is_shutdown():
-    if(not TB.rotatedFully):
-        TB.rotateTo(heading)
-    elif(not TB.reachedGoal):
-        TB.reachedGoal = TB.goToGoal(goal1)
-    # else:
-    #     TB.rotatedFully = False
-    #     TB.reachedGoal = False
+    if(not TB.rotatedFully): # if not fully rotated, then rotate
+        TB.rotateTo(TB.headingToGoal)
+    elif(not TB.reachedGoal): # if we havent reached the goal, keep driving
+        # drive function returns boolean true if turtlebot reaches goal otherwise false
+        TB.reachedGoal = TB.goToGoal(TB.distanceToGoal)
+    else:
+        print("Requesting new waypoint")
+        #request new waypoint from service
+        TB.getWaypointClient()
+    #rospy sleep
     TB.r.sleep()
